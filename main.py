a5cf2e43bd321f4672d096f15c9f8ba01c8ad47d
Delcio Cruz <delciocruzoficial@gmail.com>
2025-01-09 09:35:39 -0300

    [10] Correção de bug na interface e adicionado tratamento de error na funcionalidade enviar email

--- main.py
+++ main.py
@@ -6,14 +6,15 @@ from PIL import Image
 import pyperclip
 import random
 
-
-
-
 class App():
+    """
+    O aplicativo retorna senhas seguras, com a personalização escolhida pelo usuário. 
+    """
 
     def __init__(self) -> object:
         self.raiz = ctk.CTk()
         self._configInterface()
+        self.color_system()
         self.frame_widgets()
         self.frame_resultado()
         self.resultado_final = None
@@ -26,101 +27,112 @@ class App():
         "Configuraçoes de personalização da tela principal"
         self.raiz.title("Gerador de senha segura")
         self.raiz.geometry("556x280")
-        self.raiz.resizable(False, False)
+        self.raiz.resizable(False,False)
         self.raiz.iconbitmap("imagens/main.ico")
-        self.raiz._set_appearance_mode("light")
+        self.raiz._set_appearance_mode('light')
+    
+    def color_system(self) -> None:
+        """ Cores usadas no sistema"""
+        self.medium_gray = "#888888"
+        self.dark_gray = "#A9A9A9"
 
     def frame_widgets(self) :
-        """ Serviços de interface - Frame e checkboxs de escolhas do usuário.
-            Mapeamentodos codigos e serviços de widgets:
-                 43 Frame inicial de widgets |
-                 47 Checkbox numeros |
-                 56 Checkbox simbolos |
-                 65 Checkbox Maiusculas |
-                 74 Checkbox minusculas |
-                 83 Menu de opçoes para tamanho da senha
-        """
+        """ Serviços de interface - Frame e checkboxs de escolhas do usuário """
         # Frame inicial de widgets
-        self.frame_inicial = ctk.CTkFrame(master=self.raiz, border_width=1)
+        self.frame_inicial = ctk.CTkFrame(master=self.raiz,
+                                          corner_radius=0,
+                                          border_color=self.dark_gray,
+                                          fg_color="#D3D3D3",
+                                          border_width=1)
         self.frame_inicial.place(relx=0.54, rely=0.1, relwidth=0.41, relheight=0.8)
 
         # Checkbox numeros
         self.VarCKB_numeros = ctk.CTkCheckBox(master=self.frame_inicial,
-                                            text="Numeros",
-                                            border_width=2,
-                                            border_color="#888888",                                   
-                                            onvalue="on",
-                                            offvalue="off")
+                                              border_color= self.medium_gray,
+                                              text_color=self.medium_gray,
+                                              border_width=2,  
+                                              text="Numeros",                                 
+                                              onvalue="on",
+                                              offvalue="off")
         self.VarCKB_numeros.place(relx=0.17, rely=0.1, relwidth=0.5, relheight=0.2)
 
         # Checkbox simbolos
         self.VarCKB_simbolos = ctk.CTkCheckBox(master=self.frame_inicial,
-                                            text="Simbolos",
-                                            border_width=2,
-                                            border_color="#888888", 
-                                            onvalue="on",
-                                            offvalue="off")
+                                               text_color=self.medium_gray,
+                                               text="Simbolos",
+                                               border_width=2,
+                                               border_color=self.medium_gray, 
+                                               onvalue="on",
+                                               offvalue="off")
         self.VarCKB_simbolos.place(relx=0.17, rely=0.25, relwidth=0.5, relheight=0.2)
 
         # Checkbox Maiusculas
         self.VarCKB_maiusculas = ctk.CTkCheckBox(master=self.frame_inicial,
-                                            text="Maiusculas",
-                                            border_width=2,
-                                            border_color="#888888", 
-                                            onvalue="on",
-                                            offvalue="off")
+                                                 text_color=self.medium_gray,
+                                                 text="Maiusculas",
+                                                 border_width=2,
+                                                 border_color=self.medium_gray, 
+                                                 onvalue="on",
+                                                 offvalue="off")
         self.VarCKB_maiusculas.place(relx=0.17, rely=0.4, relwidth=0.5, relheight=0.2)
 
         # Checkbox minusculas
         self.VarCKB_minusculas = ctk.CTkCheckBox(master=self.frame_inicial,
-                                            text="Minusculas",
-                                            border_width=2,
-                                            border_color="#888888", 
-                                            onvalue="on",
-                                            offvalue="off")
+                                                 text_color=self.medium_gray,
+                                                 text="Minusculas",
+                                                 border_width=2,
+                                                 border_color=self.medium_gray, 
+                                                 onvalue="on",
+                                                 offvalue="off")
         self.VarCKB_minusculas.place(relx=0.17, rely=0.55, relwidth=0.5, relheight=0.2)
 
         # Menu de opçoes para tamanho da senha
         self.menu_opcao = ctk.CTkOptionMenu(master=self.frame_inicial,
+                                            fg_color= self.medium_gray,
                                             values=["4","5","6","7","8","9","10","11","12"])
         self.menu_opcao.place(relx=0.12, rely=0.75, relwidth=0.25, relheight=0.13)
         
         self.Label_menu_opcao = ctk.CTkLabel(master=self.frame_inicial,
-                                            text="Tamanho da senha")
+                                             text_color=self.medium_gray,
+                                             text="Tamanho da senha")
         self.Label_menu_opcao.place(relx=0.4, rely=0.75, relwidth=0.47, relheight=0.13)
     
     def frame_resultado(self) -> str:
         "Frame de saída do resultado"
-        self.frame_local = ctk.CTkFrame(master=self.raiz, border_width=1)
+        self.frame_local = ctk.CTkFrame(master=self.raiz,
+                                        corner_radius=0,
+                                        border_color=self.dark_gray,
+                                        fg_color="#D3D3D3",
+                                        border_width=1)
         self.frame_local.place(relx=0.07, rely=0.1, relwidth=0.4, relheight=0.28)
 
     def gerar_senha(self) -> (None | str):
         """ Labels, condicionais e estruturas para gerar a senha.
             Mapeamento do codigo:
-                121 Limpa tela |
-                126 Coleta e especificação requisitados na interface |
-                135 Especificação de tuplas para fornecer dados ás condicionais |
-                154 Condicional sem requisitos |
-                158 Condicional - Numeros |
-                167 Condicional - Simbolos |
-                176 Condicional - Maiusculas |
-                185 Condicional - Minusculas |
-                194 Condicional - Numeros + simbolos |
-                203 Condicional - Numeros + maiusculas |
-                212 Condicional - Numeros + minusculas |
-                221 Condicional - Numeros + simbolos + maiusculas |
-                230 Condicional - Numeros + maiusculas + minusculas |
-                239 Condicional - Simbolos + maiusculas + minusculas |
-                248 Condicional - Simbolos + maiusculas |
-                257 Condicional - Simbolos +  minusculas |
-                266 Condicional - Maiusculas + minusculas |
-                275 Condicional - Numeros + simbolos + maiusculas + minusculas |
-                284 Resposta apresentada na interface
+                Limpa tela |
+                Coleta e especificação requisitados na interface |
+                Especificação de tuplas para fornecer dados ás condicionais |
+                Condicional sem requisitos |
+                Condicional - Numeros |
+                Condicional - Simbolos |
+                Condicional - Maiusculas |
+                Condicional - Minusculas |
+                Condicional - Numeros + simbolos |
+                Condicional - Numeros + maiusculas |
+                Condicional - Numeros + minusculas |
+                Condicional - Numeros + simbolos + maiusculas |
+                Condicional - Numeros + maiusculas + minusculas |
+                Condicional - Simbolos + maiusculas + minusculas |
+                Condicional - Simbolos + maiusculas |
+                Condicional - Simbolos +  minusculas |
+                Condicional - Maiusculas + minusculas |
+                Condicional - Numeros + simbolos + maiusculas + minusculas |
+                Resposta apresentada na interface
         """
         
         # Limpa tela
         self.label_resposta = ctk.CTkLabel(self.frame_local,
-                                               text=" \t\t\t  ")
+                                           text=" \t\t\t  ")
         self.label_resposta.place(relx=0.17 ,rely=0.35)
 
         # Coleta e especificação requisitados na interface
@@ -283,49 +295,65 @@ class App():
     
         # Resposta apresentada na interface  
         self.label_resposta = ctk.CTkLabel(self.frame_local,
-                                               text=f"{self.resultado_final}")
+                                           text_color='black',
+                                           text=f"{self.resultado_final}")
         self.label_resposta.place(relx=0.3 ,rely=0.35)
     
     def botao_requisitar_senha(self) -> None:
-
+        """ Botão chama o metodo gerar senha """
         self.imagem_requisitar = ctk.CTkImage(light_image=Image.open("./imagens/cadeado.png"), 
-                                            dark_image=Image.open("./imagens/cadeado.png"),
-                                            size=(30,25))
+                                              dark_image=Image.open("./imagens/cadeado.png"),
+                                              size=(30,25))
         self.btt_requistar = ctk.CTkButton(master=self.raiz,
-                                        fg_color="#4169E1",
-                                        border_width=1,
-                                        border_color="#A9A9A9",
-                                        text="Gerar senha segura",
-                                        image=self.imagem_requisitar,
-                                        command=self.gerar_senha)
+                                           corner_radius=0,
+                                           fg_color="#4169E1",
+                                           border_width=1,
+                                           border_color=self.dark_gray,
+                                           text="Gerar senha segura",
+                                           image=self.imagem_requisitar,
+                                           command=self.gerar_senha)
         self.btt_requistar.place(relx=0.07, rely=0.55, relwidth=0.4, relheight=0.15)
         
     def _enviar_email(self) -> (str | None):
         """A classe tem a funcionalidade de integrar as contas (Gmail, Hotmail, Outlook) logadas no outlook, para enviar o feedback do usuário 
         ao desenvolvedor. Método | Enviar( ) Configuraçoes nescessarias para enviar email ao desenvolvedor do sistema |
         """
-        self._outlook = win32.Dispatch("outlook.application") # Integração python outlook
-        self._email = self._outlook.CreateItem(0) # Instancia no outlook
-        self.caixa_dialogo = ctk.CTkInputDialog(title="Feedback",
-                                                text="\nEste sistema é seguro!\nDesde já agradecemos sua colaboração!\nEscreva seu feedback abaixo!")
-        self.feedback = self.caixa_dialogo.get_input()
-        self._email.To = "delciocruzoficial@gmail.com"
-        self._email.Subject = "Feedback - Gerador de senhas"
-        self._email.Body = f"{self.feedback}"
-        self._email.Send()
+        try:
+            _outlook = win32.Dispatch("outlook.application") # Integração python outlook
+            _email = _outlook.CreateItem(0) # Instancia no outlook
+            self.caixa_dialogo = ctk.CTkInputDialog(title="Feedback",
+                                                    text="\nEste sistema é seguro!\nDesde já agradecemos sua colaboração!\nEscreva seu feedback abaixo!")
+            feedback = self.caixa_dialogo.get_input()
+
+            if feedback is None or feedback.strip() == "":
+                return
+
+            _email.To = "delciocruzoficial@gmail.com"
+            _email.Subject = "Feedback - Gerador de senhas"
+            _email.Body = f"{feedback}"
+            _email.Send()
+
+        except ConnectionError as ce:
+            raise Exception(f'Error de conexão com o outlook: {str(ce)}')
+        except Exception as e:
+            raise Exception(f'Algo inesperado aconteceu: {str(e)}')
+    
+
 
     def botao_feedback(self):
         """ Botão responsavel pela chamada do feedback"""
         self.imagem_outlook = ctk.CTkImage(light_image=Image.open("./imagens/outlook.png"),
-                                        dark_image=Image.open("./imagens/outlook.png"),
-                                        size=(35,25))
+                                           dark_image=Image.open("./imagens/outlook.png"),
+                                           size=(35,25))
+                                           
         self.btt_feedback = ctk.CTkButton(master=self.raiz,
-                                        fg_color="#4169E1",
-                                        border_width=1,
-                                        border_color="#C0C0C0",
-                                        text="Feedback via outlook",
-                                        image=self.imagem_outlook,
-                                        command= self._enviar_email)
+                                         corner_radius=0,
+                                         fg_color="#4169E1",
+                                         border_width=1,
+                                         border_color="#C0C0C0",
+                                         text="Feedback via outlook",
+                                         image=self.imagem_outlook,
+                                         command= self._enviar_email)
         self.btt_feedback.place(relx=0.07, rely=0.74, relwidth=0.4, relheight=0.15)
 
     def area_trasnferencia(self) -> str:
@@ -338,12 +366,13 @@ class App():
     def botao_areaTrasferencia(self):
         """Botão de copiar para area de transferencia"""
         self.btt_areaTrasferencia = ctk.CTkButton(master=self.raiz,
-                                        fg_color="#D3D3D3",
-                                        border_width=1,
-                                        border_color="#A9A9A9",
-                                        text_color="#A9A9A9",
-                                        text="Copiar para area de transferencia",
-                                        command=self.area_trasnferencia)
+                                                  fg_color="#D3D3D3",
+                                                  border_width=1,
+                                                  corner_radius=0,
+                                                  border_color=self.dark_gray,
+                                                  text_color=self.dark_gray,
+                                                  text="Copiar para area de transferencia",
+                                                  command= self.area_trasnferencia)
         self.btt_areaTrasferencia.place(relx=0.07, rely=0.4, relwidth=0.4, relheight=0.1)
 
 
